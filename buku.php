46f5436411734bd4fe346fc8471a6e4e8c235498
stragnantt <keviin11223345@gmail.com>
2025-07-30 09:04:55 +0700

    bener buku.php

--- buku.php
+++ buku.php
@@ -1,38 +1,48 @@
 <?php
- class Buku {
-     public $judul;
-     public $pengarang;
-     public $penerbit;
-     public $tahun;
+class Buku {
+    public $judul;
+    public $pengarang;
+    public $penerbit;
+    public $tahun;
 
-     public function __construct($judul, $pengarang, $penerbit, $tahun) {
-         $this->judul = $judul;
-         $this->pengarang = $pengarang;
-         $this->penerbit = $penerbit;
-         $this->tahun = $tahun;
-     }
-
-     public function infoBuku() {
-         echo "<h1>Informasi Buku</h1>";
-         echo "Judul: " . $this->judul . "<br>";
-         echo "Pengarang: " . $this->pengarang . "<br>";
-         echo "Penerbit: " . $this->penerbit . "<br>";
-         echo "Tahun Terbit: " . $this->tahun . "<br>";
-     }
- }
-
-    $buku1 = new Buku("Laskar Pelangi", "Andrea Hirata", "Bentang Pustaka", 2005);
-
-    $buku2 = new Buku("Perahu Kertas", "Dee Lestari", " Bentang Pustaka",  2009);
-
-    $buku3 = new Buku("Amba", " Laksmi Pamuntjak", "Gramedia Pustaka Utama", 2012);
-
-    $buku4 = new Buku("Supernova: Ksatria, Puteri, dan Bintang Jatuh", " Dee Lestari", " Truedee Books",  2001);
-
-    $buku5 = new Buku("Cantik Itu Luka", " Eka Kurniawan", "Gramedia Pustaka Utama", 2002);
-    
-    $semuabuku = array($buku1, $buku2, $buku3, $buku4, $buku5);
-    
-    foreach ($semuabuku as $buku) {
-        $buku->infoBuku();
+    public function __construct($judul, $pengarang, $penerbit, $tahun) {
+        $this->judul = $judul;
+        $this->pengarang = $pengarang;
+        $this->penerbit = $penerbit;
+        $this->tahun = $tahun;
     }
+
+    public function infoBuku() {
+        echo "<h1>Informasi Buku</h1>";
+        echo "Judul: " . $this->judul . "<br>";
+        echo "Pengarang: " . $this->pengarang . "<br>";
+        echo "Penerbit: " . $this->penerbit . "<br>";
+        echo "Tahun Terbit: " . $this->tahun . "<br>";
+        echo $this->tahunTerbit();
+        echo "<br>";
+    }
+
+    public function tahunTerbit() {
+        if ($this->tahun < 2000) {
+            return "Buku ini terbit sebelum tahun 2000.<br>";
+        } else {
+            return "Buku ini terbit setelah tahun 2000.<br>";
+        }
+    }
+
+    public function __destruct() {
+        echo "Buku '{$this->judul}' telah dihapus.<br>";
+    }
+}
+
+$semuabuku = [
+    new Buku("Sang Pemimpi", "Andrea Hirata", "Bentang Pustaka", 2006),
+    new Buku("Pulang", "Leila S. Chudori", "KPG", 2012),
+    new Buku("Bumi Manusia", "Pramoedya Ananta Toer", "Hasta Mitra", 1980),
+    new Buku("Anak Semua Bangsa", "Pramoedya Ananta Toer", "Hasta Mitra", 1980),
+    new Buku("Jejak Langkah", "Pramoedya Ananta Toer", "Hasta Mitra", 1985)
+];
+
+foreach ($semuabuku as $buku) {
+    $buku->infoBuku();
+}
